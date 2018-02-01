1bf42f4cd8137e2aa74da378f6d7b8b8e8b72132
Manvendra Singh <manvendrask2@gmail.com>
2018-02-01 23:52:31 +0530

    Added help modal sheet.

--- src/comps/SignupConfirm.js
+++ src/comps/SignupConfirm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View, Text, TouchableOpacity, TextInput } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput, Modal } from 'react-native';
 
 class SignupConfirm extends Component {
   static propTypes = {
@@ -11,8 +11,14 @@ class SignupConfirm extends Component {
     header: null
   });
 
+  constructor(props) {
+    super(props);
+    this.state = { help: false };
+  }
+
   render() {
-    const { params } = this.props.navigation.state;
+    const { navigation } = this.props;
+    const { params } = navigation.state;
 
     return (
       <View style={{ flex: 1, justifyContent: 'space-between' }}>
@@ -26,13 +32,28 @@ class SignupConfirm extends Component {
         </View>
 
         <View style={{ flexDirection: 'row' }}>
-          <TouchableOpacity style={{ flex: 1, backgroundColor: 'white', padding: 21 }}>
-            <Text style={{ textAlign: 'center' }}>Continue</Text>
+          <TouchableOpacity style={{ flex: 1, borderRightWidth: 1, backgroundColor: 'white', padding: 21 }}
+            onPress={() => this.setState({ help: true })}>
+            <Text style={{ textAlign: 'center' }}>Help</Text>
           </TouchableOpacity>
           <TouchableOpacity style={{ flex: 1, backgroundColor: 'white', padding: 21 }}>
             <Text style={{ textAlign: 'center' }}>Continue</Text>
           </TouchableOpacity>
         </View>
+
+        <Modal animationType="fade" visible={this.state.help} transparent
+          onRequestClose={() => this.setState({ help: false })}>
+          <View style={{ flex: 1, justifyContent: 'flex-end' }}>
+            <TouchableOpacity style={{ borderBottomWidth: 1, backgroundColor: 'white', padding: 21 }}
+              onPress={() => navigation.goBack()}>
+              <Text>Edit number/email</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{ backgroundColor: 'white', padding: 21 }}
+              onPress={() => this.setState({ help: false })}>
+              <Text>Resend code</Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
       </View>
     );
   }
